9a17384f3b7419fe75a0585e628e0f6cd59ede67
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-06-18 20:09:31 +0200

    Add project files.

---
+++ SchoolAdmin/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Data;
+
+namespace SchoolAdmin
+{
+    internal class Program
+    {
+        enum Weekdagen { Maandag, Dinsdag, Woensdag, Donderdag, Vrijdag, Zaterdag, Zondag };
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            Student said = new();
+            Student mieke = new();
+
+
+            said.Name = "Said Aziz";
+            said.BirthDate = new DateTime(2000, 12, 23);
+            said.StudentNumber = Student.StudentCounter;
+            said.Courses.Add("Programmeren");
+            said.Courses.Add("Databanken");
+            Student.StudentCounter++;
+
+
+
+            mieke.Name = "Mike Vermeulen";
+            mieke.BirthDate = new DateTime(1998, 1, 1);
+            mieke.StudentNumber = Student.StudentCounter;
+            mieke.Courses.Add("Programmeren");
+            mieke.Courses.Add("Databanken");
+            Student.StudentCounter++;
+
+
+
+
+
+        }
+
+    }
+}
---
+++ SchoolAdmin/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin
+{
+    internal class Student
+    {
+
+        public string Name;
+        public DateTime BirthDate;
+        public uint StudentNumber;
+        public List<string> Courses = new();
+        public static uint StudentCounter = 1;
+       
+    }
+}
